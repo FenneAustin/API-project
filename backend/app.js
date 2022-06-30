ed531d12fdd1d6be7cada3cc1bc71a1ff01a2e00
Austin <austinfenne@icloud.com>
2022-06-30 16:45:10 -0700

    added to model

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ const app = express();
 
 const routes = require("./routes");
 
+const { ValidationError } = require("sequelize");
 
 app.use(morgan("dev"));
 
@@ -46,13 +47,24 @@ app.use(
 );
 
 
-
-
-
-
 app.use(routes); // Connect all the routes
 
 
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
 
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
 
 module.exports = app;
